20adbf8f63616077c899b7c386446ad2ac06fcbc
emsthf <deunsol916@gmail.com>
2021-12-28 12:31:39 +0900

    UserController

--- jparepositorystudy/src/main/java/unionjpa/JparepositorystudyApplication.java
+++ jparepositorystudy/src/main/java/unionjpa/JparepositorystudyApplication.java
@@ -1,7 +1,11 @@
 package unionjpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import unionjpa.user.model.User;
+import unionjpa.user.service.UserService;
 
 @SpringBootApplication
 public class JparepositorystudyApplication {
@@ -10,4 +14,29 @@ public class JparepositorystudyApplication {
 		SpringApplication.run(JparepositorystudyApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserService userService) {   // 스프링이 실행될 때 같이 실행 됨
+		return args -> {
+			userService.addUser(User.builder()
+					.userName("Tim Cook")
+					.password("qwer1234")
+					.build());
+			userService.addUser(User.builder()
+					.userName("Steve Jobs")
+					.password("123asdfg")
+					.build());
+			userService.addUser(User.builder()
+					.userName("John Kim")
+					.password("52939zzz")
+					.build());
+			userService.addUser(User.builder()
+					.userName("The Weeknd")
+					.password("gjkdfo111")
+					.build());
+			userService.addUser(User.builder()
+					.userName("Khalid")
+					.password("doowjd222")
+					.build());
+		};
+	}
 }
--- jparepositorystudy/src/main/java/unionjpa/user/model/User.java
+++ jparepositorystudy/src/main/java/unionjpa/user/model/User.java
@@ -1,13 +1,16 @@
 package unionjpa.user.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
+@Data   // 데이터 어노테이션은 모두 있는 생성자와 게터/세터만 만들어줌
+@NoArgsConstructor   // 빈 생성자 만들어주는 어노테이션
 @Entity
 public class User {
     @Id
@@ -15,4 +18,10 @@ public class User {
     private Long id;
     private String userName;
     private String password;
+
+    @Builder
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 }
