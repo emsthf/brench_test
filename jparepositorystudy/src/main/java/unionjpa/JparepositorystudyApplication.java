7d1b52e254c2475d51e899f9051058a5e3e0a1e0
emsthf <deunsol916@gmail.com>
2021-12-28 09:33:22 +0900

    new

---
+++ jparepositorystudy/src/main/java/unionjpa/JparepositorystudyApplication.java
@@ -0,0 +1,13 @@
+package unionjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparepositorystudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparepositorystudyApplication.class, args);
+	}
+
+}
---
+++ jparepositorystudy/src/test/java/unionjpa/JparepositorystudyApplicationTests.java
@@ -0,0 +1,13 @@
+package unionjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JparepositorystudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
